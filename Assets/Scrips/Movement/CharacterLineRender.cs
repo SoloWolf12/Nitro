98cc441ee46703505d1f629c78327dd440e8db4e
SoloWolf <danielaguerreberry@live.com.ar>
2023-02-12 15:11:21 -0300

    aa
    adsd

---
+++ Assets/Scrips/Movement/CharacterLineRender.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLineRender : MonoBehaviour
+{
+    [SerializeField] float detectLevelHeiht;  //distancia del rayo, hay uno para adelante o cruzar uno de forma diagonal como un parachoques?
+    [SerializeField]private LayerMask _layerMask;
+    private RaycastHit _hitCollider;
+    [SerializeField] private float ascencionSpeed;
+    [SerializeField] private GameObject ShipModel;
+
+
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out _hitCollider, detectLevelHeiht, _layerMask))
+        {
+            Debug.DrawLine(transform.position, _hitCollider.point, Color.blue);
+            Debug.Log("go up NOW!!");
+            ShipModel.transform.position += transform.position.y * new Vector3(0, -1, 0) * (ascencionSpeed * Time.deltaTime);
+        }
+    }
+
+}
---
+++ Assets/Scrips/Movement/CharacterLineRenderDown.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLineRenderDown : MonoBehaviour
+{
+    [SerializeField] float detectLevelHeiht;  
+    [SerializeField] private LayerMask _layerMask;
+    private RaycastHit _hitCollider;
+    [SerializeField] private float descendSpeed;
+    [SerializeField] private GameObject ShipModel;
+
+
+    void Update()
+    {
+
+        if (Physics.Raycast(transform.position, Vector3.down, out _hitCollider, detectLevelHeiht, _layerMask))
+        {
+            Debug.Log("Is not safe to descend");
+            Debug.DrawLine(transform.position, transform.position + Vector3.down * 15f, Color.red);
+        }
+        else 
+        {
+            Debug.Log("keep descending");       
+            ShipModel.transform.localPosition+=  new Vector3(0, -1, 0) * (descendSpeed * Time.deltaTime);
+    }
+
+    }
+}
